2726ff1d14013c550115f639f8254f1c393bdb02
hiroaki_seto <hiroaki5141616@me.com>
2022-12-07 21:53:26 +0900

    ユーザ詳細画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :users, only: %i[index new create]
+  resources :users, except: %i[edit update destroy]
 end
