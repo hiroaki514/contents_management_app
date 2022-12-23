02cfc56913c0457024a41ef95b4c6c99dd7b1f5d
hiroaki_seto <hiroaki5141616@me.com>
2022-12-23 23:07:05 +0900

    edit,updateの実装途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.save
+    redirect_to root_path
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to root_path
   end
 
   private
