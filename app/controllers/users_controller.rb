e96d4ac63f091a91e63b4a88e125d76efa4fc172
hiroaki_seto <hiroaki5141616@me.com>
2022-12-27 20:23:36 +0900

    コントローラのリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
   def index
     @users = User.all
   end
@@ -15,16 +17,11 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to root_path
   end
@@ -33,5 +30,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :belonging, :role)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
