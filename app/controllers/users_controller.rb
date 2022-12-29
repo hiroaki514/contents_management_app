278a968ae1611a4f7355545295197a8967ebd0a9
hiroaki_seto <hiroaki5141616@me.com>
2022-12-29 20:53:30 +0900

    フォームのバリデーションチェックを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to root_path
+    if @user.valid?
+      @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show; end
