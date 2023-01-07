be93ea007b44703cf3b020208b34af7f69451b43
hiroaki_seto <hiroaki5141616@me.com>
2023-01-07 13:58:05 +0900

    devise実装のため一旦初期に戻した

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :recoverable, :rememberable, :validatable
-  devise :database_authenticatable, :registerable
-  validates :name, :email, :role, presence: true
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_04_112301) do
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
-    t.string "name", comment: "ユーザー名"
-    t.string "belonging", comment: "所属"
-    t.string "role", comment: "権限"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
