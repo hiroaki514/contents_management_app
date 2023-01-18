bd51ab47e1849f9f81eac1138c93d71ebc0ef613
hiroaki_seto <hiroaki5141616@me.com>
2023-01-18 20:38:17 +0900

    マイグレーションファイルの追加

---
+++ db/migrate/20230118111633_add_details_to_users.rb
@@ -0,0 +1,7 @@
+class AddDetailsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :failed_attempts, :integer, default: 0, null: false, after: :remember_created_at
+    add_column :users, :unlock_token, :string, after: :failed_attempts
+    add_column :users, :locked_at, :datetime, after: :unlock_token
+  end
+end
