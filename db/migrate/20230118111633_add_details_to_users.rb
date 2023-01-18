9457fe91c1c36cc4195478970fc0bc39c0a07e80
hiroaki_seto <hiroaki5141616@me.com>
2023-01-18 20:55:51 +0900

    パスワード失敗時のロックの詳細を追加

--- db/migrate/20230118111633_add_details_to_users.rb
+++ db/migrate/20230118111633_add_details_to_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddDetailsToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :failed_attempts, :integer, default: 0, null: false, after: :remember_created_at
