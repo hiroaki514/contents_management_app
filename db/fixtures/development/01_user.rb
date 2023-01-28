e4432d2bdd8ff011a0cc0f8a086a9a63041d36ab
hiroaki_seto <hiroaki5141616@me.com>
2023-01-28 13:14:35 +0900

    現時点で不要なseed_fu情報を削除

--- db/fixtures/development/01_user.rb
+++ db/fixtures/development/01_user.rb
@@ -2,25 +2,16 @@ User.seed do |s|
   s.id    = 1
   s.email = 'hiroaki5141616@me.com'
   s.password = 'password123'
-  s.name = '瀬戸博章'
-  s.name_kana = 'セトヒロアキ'
-  s.phone_number = '08011112222'
 end
 
 User.seed do |s|
   s.id    = 2
   s.email = 'hoge@example.com'
   s.password = 'Hoge1234!'
-  s.name = '田中太郎'
-  s.name_kana = 'タナカタロウ'
-  s.phone_number = '09011112222'
 end
 
 User.seed do |s|
   s.id    = 3
   s.email = 'hogehoge@example.com'
   s.password = 'HogeHoge1234!'
-  s.name = '田中二郎'
-  s.name_kana = 'タナカジロウ'
-  s.phone_number = '08011112222'
 end
