6486a0d7e88d091dd70b4df52f348ee40f09303f
hiroaki_seto <hiroaki5141616@me.com>
2023-02-01 20:21:31 +0900

    カラムの順番変更

--- db/migrate/20230128060413_add_name_to_users.rb
+++ db/migrate/20230128060413_add_name_to_users.rb
@@ -2,6 +2,6 @@
 
 class AddNameToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :name, :string, null: false, before: :email
+    add_column :users, :name, :string, null: false, after: :encrypted_password
   end
 end
