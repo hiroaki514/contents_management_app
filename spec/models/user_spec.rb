eb0d86648b7d219f3d2c4b19ea7107307768aa1e
hiroaki_seto <hiroaki5141616@me.com>
2023-02-20 03:26:54 +0900

    Rspecのテスト挙動追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,20 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "名前の入力が必須であること" do
+  it '名前の入力が必須であること' do
     user = User.new(
       name: ''
     )
     expect(user).to be_invalid
-    
+
     # user = User.new(
     #   name: 'テスト太郎'
     # )
     # expect(user).to be_valid
   end
 
-  it "権限の選択が必須であること" do
-
+  it '権限の選択が必須であること' do
   end
-
-
 end
