3810ed4affe663a053bd4ffece24d1da11170b79
hiroaki_seto <hiroaki5141616@me.com>
2023-02-27 20:37:41 +0900

    ログイン後の遷移画面を一般ユーザ画面に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # MEMO: :"authenticate_user!"はログインしていない場合ログインページに飛ばす記述
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -9,4 +10,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name role])
   end
+
+  def after_sign_in_path_for(resource)
+    general_index_path
+  end
+
 end
