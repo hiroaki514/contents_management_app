a365f0b690a8f8789e951c1483d18bdb57f1761f
hiroaki_seto <hiroaki5141616@me.com>
2023-04-13 00:05:38 +0900

    iconアップロード時の拡張子についてバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   enum role: { master: 0, admin: 1, general: 2 }
 
   validates :name, :role, presence: true
-  validate :file_size
+  validate :file_size, :file_extension
   has_one_attached :icon
 
 
@@ -18,7 +18,13 @@ class User < ApplicationRecord
   def file_size
     # MEMO: attached?とblobとbyte_sizeはActiveStorageのメソッド、megabytesはRailsのメソッド
     if icon.attached? && icon.blob.byte_size > 5.megabytes
-      errors.add(:icon, 'は5MB以下にしてください')
+      errors.add(:icon, 'は5MB以下のファイルをアップロードしてください')
+    end
+  end
+
+  def file_extension
+    if icon.attached? && icon.blob.image? && !['.jpg', '.jpeg', '.png'].include?(File.extname(icon.blob.filename.to_s).downcase)
+      errors.add(:icon, 'はJPEGまたはPNG形式のファイルをアップロードしてください')
     end
   end
 
