b0490ec579873426f3b5fe5a5aa89d4eaf209f81
hiroaki_seto <hiroaki5141616@me.com>
2023-05-06 16:33:53 +0900

    test用の01_user.rbを追記

--- db/fixtures/test/01_user.rb
+++ db/fixtures/test/01_user.rb
@@ -7,3 +7,35 @@ User.seed do |s|
   s.password = 'password123'
   s.role = 0
 end
+
+User.seed do |s|
+  s.id = 2
+  s.name = '穂夏太郎'
+  s.email = 'hoge@example.com'
+  s.password = 'Hoge1234!'
+  s.role = 1
+end
+
+User.seed do |s|
+  s.id = 3
+  s.name = '穂夏次郎'
+  s.email = 'hogehoge@example.com'
+  s.password = 'HogeHoge1234!'
+  s.role = 2
+end
+
+User.seed do |s|
+  s.id = 4
+  s.name = '穂夏三郎'
+  s.email = 'hogehogehoge@example.com'
+  s.password = 'HogeHoge1234!'
+  s.role = 2
+end
+
+User.seed do |s|
+  s.id = 5
+  s.name = '穂夏四郎'
+  s.email = 'hogehogehogehoge@example.com'
+  s.password = 'HogeHoge1234!'
+  s.role = 2
+end
