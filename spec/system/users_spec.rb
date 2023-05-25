86cc8dbfe6e341fcf108c77516861b8430d552ba
yoshichiba <yoshi19941117@gmail.com>
2023-05-25 23:12:59 +0900

    seedファイルとSystemSpecを修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -11,15 +11,136 @@ RSpec.describe 'Users', type: :system do
       end
     end
 
-    context 'ログイン状態の場合' do
-      it '正常にログイン後のページへ飛ぶこと' do
-        visit new_user_session_path
-        expect(page).to have_content('Content Management App')
+    xcontext 'ログイン情報に不備がある場合' do
+      it 'エラーメッセージと共にログインページへ戻されること' do
+      end
+    end
 
-        fill_in 'user[email]', with: 'hiroaki5141616@me.com'
-        fill_in 'user[password]', with: 'password123'
-        click_on 'ログイン'
-        expect(page).to have_content('ログインしました。')
+    context 'ログイン状態の場合' do
+      context 'マスター権限の場合' do
+        before do
+          visit new_user_session_path
+          fill_in 'user[email]', with: 'hiroaki5141616@me.com'
+          fill_in 'user[password]', with: 'password123'
+          click_on 'ログイン'
+        end
+
+        it 'マスター画面と管理者画面へのリンクが表示されること' do
+          expect(page).to have_link('マスター画面')
+          expect(page).to have_link('管理者画面')
+        end
+
+        context 'ログアウトする場合' do
+          before do
+            click_on 'ログアウト'
+          end
+
+          it 'ログインページに遷移すること' do
+            expect(page).to have_content('ログイン')
+          end
+        end
+
+        context 'マスター画面へ遷移する場合' do
+          before do
+            click_on 'マスター画面'
+          end
+
+          it '一般ユーザ画面のリンクが表示されていること' do
+            expect(page).to have_link('一般ユーザ画面')
+          end
+
+          it 'マスター画面のリンクが表示されていないこと' do
+            expect(page).not_to have_link('マスター画面')
+          end
+
+          it 'ユーザが表示されること' do
+            expect(page).to have_content('穂夏太郎')
+            expect(page).to have_content('hiroaki5141616@me.com')
+            expect(page).to have_content('マスター')
+          end
+        end
+
+        context '管理者画面へ遷移する場合' do
+          before do
+            click_on '管理者画面'
+          end
+
+          it '一般ユーザ画面のリンクが表示されていること' do
+            expect(page).to have_link('一般ユーザ画面')
+          end
+
+          it '管理者画面のリンクが表示されていないこと' do
+            expect(page).not_to have_link('管理者画面')
+          end
+        end
+      end
+
+      context '管理者権限の場合' do
+        before do
+          visit new_user_session_path
+          fill_in 'user[email]', with: 'hoge@example.com'
+          fill_in 'user[password]', with: 'password123'
+          click_on 'ログイン'
+        end
+
+        it '管理者画面とマスター画面へのリンクが表示されること' do
+          expect(page).not_to have_link('マスター画面')
+          expect(page).to have_link('管理者画面')
+        end
+
+        context 'ログアウトする場合' do
+          before do
+            click_on 'ログアウト'
+          end
+
+          it 'ログインページに遷移すること' do
+            expect(page).to have_content('ログイン')
+          end
+        end
+
+        context '管理者画面へ遷移する場合' do
+          before do
+            click_on '管理者画面'
+          end
+
+          it '一般ユーザ画面のリンクが表示されていること' do
+            expect(page).to have_link('一般ユーザ画面')
+          end
+
+          it '管理者画面のリンクが表示されていないこと' do
+            expect(page).not_to have_link('管理者画面')
+          end
+
+          it 'ユーザが表示されること' do
+            expect(page).to have_content('穂夏次郎')
+            expect(page).to have_content('hoge@example.com')
+            expect(page).to have_content('管理者')
+          end
+        end
+      end
+
+      context '一般ユーザ権限の場合' do
+        before do
+          visit new_user_session_path
+          fill_in 'user[email]', with: 'hogehoge@example.com'
+          fill_in 'user[password]', with: 'password123'
+          click_on 'ログイン'
+        end
+
+        it '管理者画面とマスター画面へのリンクが表示されないこと' do
+          expect(page).not_to have_link('マスター画面')
+          expect(page).not_to have_link('管理者画面')
+        end
+
+        context 'ログアウトする場合' do
+          before do
+            click_on 'ログアウト'
+          end
+
+          it 'ログインページに遷移すること' do
+            expect(page).to have_content('ログイン')
+          end
+        end
       end
     end
   end
