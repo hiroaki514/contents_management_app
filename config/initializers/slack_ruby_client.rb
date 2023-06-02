f555c8798be7ec4573616cf35fe7b5c9fb7dc787
hiroaki_seto <hiroaki5141616@me.com>
2023-06-02 20:15:04 +0900

    credentialファイルの編集とその他追記

--- config/initializers/slack_ruby_client.rb
+++ config/initializers/slack_ruby_client.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Slack.configure do |config|
-  config.token = xoxb-4062611786133-5350206670262-ZqeGPH56KdeS5bXejmzYn9Qk
+  config.token = Rails.application.credentials.dig(:slack, :secret_token)
 end
