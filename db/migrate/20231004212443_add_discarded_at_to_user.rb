d272fc0c82ba879d5defe544ac6561d320801e9a
hiroaki_seto <hiroaki5141616@me.com>
2023-10-11 15:46:53 +0900

    discarded_atカラムをcreated_atの前に配置するよう修正

--- db/migrate/20231004212443_add_discarded_at_to_user.rb
+++ db/migrate/20231004212443_add_discarded_at_to_user.rb
@@ -1,5 +1,5 @@
 class AddDiscardedAtToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :discarded_at, :datetime, after: :name, comment: '削除日時'
+    add_column :users, :discarded_at, :datetime, before: :created_at, comment: '削除日時'
   end
 end
