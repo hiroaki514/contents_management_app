08c85a29b04437f30afcbc0de0467bd7af861710
hiroaki_seto <hiroaki5141616@me.com>
2023-11-15 15:56:45 +0900

    操作ログに論理削除されたユーザを表示するよう追加

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,6 +2,10 @@
 
 class LogsController < ApplicationController
   def index
-    @organizations = Organization.unscope(:where).where.not(discarded_at: nil)
+    @organization_logs = Organization.unscope(:where).where.not(discarded_at: nil)
+    @user_logs = User.unscope(:where).where.not(discarded_at: nil)
+    
+    # 表示させるデータをソートできるように以下で配列を作成
+    @logs = (@organization_logs + @user_logs).sort_by(&:discarded_at).reverse
   end
 end
