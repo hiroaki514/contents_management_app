2cd2daec314a4f56e4937a055ba617bf5ee25c72
hiroaki_seto <hiroaki5141616@me.com>
2023-11-15 17:18:13 +0900

    ログの表示を絞り込みできるようにした

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,10 +2,25 @@
 
 class LogsController < ApplicationController
   def index
-    @organization_logs = Organization.unscope(:where).where.not(discarded_at: nil)
-    @user_logs = User.unscope(:where).where.not(discarded_at: nil)
-    
-    # 表示させるデータをソートできるように以下で配列を作成
-    @logs = (@organization_logs + @user_logs).sort_by(&:discarded_at).reverse
+    # case ステートメントでログのタイプ毎に分岐
+    case params[:log_type]
+    when 'organization'
+      @logs = fetch_logs(Organization)
+    when 'user'
+      @logs = fetch_logs(User)
+    else
+      @logs = Organization.unscope(:where).where.not(discarded_at: nil) + User.unscope(:where).where.not(discarded_at: nil)
+    end
+
+    # 表示用に @logs 内のデータをソートする
+    @logs = @logs.sort_by(&:discarded_at).reverse
   end
+
+  private
+
+  # モデルに記述したデフォルトスコープを無視して discarded_at が nil でないものを取得
+  def fetch_logs(model)
+    model.unscope(:where).where.not(discarded_at: nil)
+  end
+
 end
