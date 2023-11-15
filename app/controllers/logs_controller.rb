d779cf11dba43ebc7066a0ca846ff21c83ad2501
hiroaki_seto <hiroaki5141616@me.com>
2023-11-15 20:54:30 +0900

    記述の修正

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class LogsController < ApplicationController
+  before_action :fetch_log_types, only: %i[index]
   def index
+    # 表示用に @logs 内のデータをソートする
+    @logs = @logs.sort_by(&:discarded_at).reverse
+  end
+
+
+  private
+
+  def fetch_log_types
     # case でログのタイプ毎に分岐
     case params[:log_type]
     when 'organization'
@@ -11,12 +20,7 @@ class LogsController < ApplicationController
     else
       @logs = Organization.unscope(:where).where.not(discarded_at: nil) + User.unscope(:where).where.not(discarded_at: nil)
     end
-
-    # 表示用に @logs 内のデータをソートする
-    @logs = @logs.sort_by(&:discarded_at).reverse
   end
-
-  private
 
   # モデルに記述したデフォルトスコープを無視して discarded_at が nil でないものを取得
   def fetch_logs(model)
