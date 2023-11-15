e35264a78cf3661bb82a701b5f47e9101f22861b
hiroaki_seto <hiroaki5141616@me.com>
2023-11-15 20:15:01 +0900

    記述の修正

--- db/fixtures/test/01_organization.rb
+++ db/fixtures/test/01_organization.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
-Organization.seed do |s|
-  s.id = 1
-  s.name = '組織1'
-end
-
-(2..15).each do |id|
-  User.seed do |s|
+(1..15).each do |id|
+  Organization.seed do |s|
     s.id = id
     s.name = "組織#{id}"
   end
