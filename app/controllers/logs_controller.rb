627f09b512a250da65df414d578c97164c07976e
hiroaki_seto <hiroaki5141616@me.com>
2023-11-25 15:51:16 +0900

    logsコントローラー内でメソッドの整理、userとorganizationのログ処理を分けた

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -13,25 +13,26 @@ class LogsController < ApplicationController
     # case でログのタイプ毎に分岐
     @logs = case params[:log_type]
             when 'organization'
-              # fetch_logs_with_created_user_id(Organization) +
-              # fetch_logs_with_discarded_at(Organization) +
-              # fetch_logs_with_created_at(Organization)
+              organization_logs
             when 'user'
-              fetch_logs_with_created_user_id(User) +
-              fetch_logs_with_discarded_at(User) +
-              fetch_logs_with_created_at(User)
+              user_logs
             else
               all_logs
             end
   end
 
   def all_logs
-    # fetch_logs_with_created_user_id(Organization) +
-    # fetch_logs_with_discarded_at(Organization) +
-    # fetch_logs_with_created_at(Organization)
-    fetch_logs_with_created_user_id(User) +
+    (user_logs + organization_logs)
+  end
+
+  def user_logs
     fetch_logs_with_discarded_at(User) +
-    fetch_logs_with_created_at(User)
+    fetch_logs_with_created_user_id(User)
+  end
+
+  def organization_logs
+    fetch_logs_with_discarded_at(Organization) +
+    fetch_logs_with_created_user_id(Organization)
   end
 
   # モデルに記述したデフォルトスコープを無視して discarded_at が nil でないものを取得
@@ -43,8 +44,5 @@ class LogsController < ApplicationController
     model.where.not(created_user_id: nil)
   end
 
-  def fetch_logs_with_created_at(model)
-    model.where.not(created_at: nil)
-  end
 
 end
